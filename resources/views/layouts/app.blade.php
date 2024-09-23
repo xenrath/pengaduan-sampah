412e738871e60fa3e1abde383c6acba70bbde1aa
IlhamMuzani <ilhammuzani21@gmail.com>
2024-09-23 21:46:00 +0700

    menambah info hubungi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,10 +54,39 @@
             <!-- Left navbar links -->
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" data-widget="pushmenu" href="#" role="button">
-                        <i class="fas fa-bars"></i>
-                    </a>
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i
+                            class="fas fa-bars"></i></a>
                 </li>
+                <li class="nav-item d-none d-sm-inline-block">
+                    <a href="index3.html" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item d-none d-sm-inline-block">
+                    <a href="#" class="nav-link">Contact</a>
+                </li>
+            </ul>
+
+            <!-- Right navbar links -->
+            <ul class="navbar-nav ml-auto">
+
+                <!-- Messages Dropdown Menu -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link" data-toggle="dropdown" href="#">
+                        <i class="fas fa-info-circle"></i> <span class="badge badge-danger navbar-badge"></span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                        <a href="#" class="dropdown-item">
+                            <!-- Message Start -->
+                            <div>
+                                <h3 style="font-size:13px">Jika ada memerlukan bantuan lebih lanjut:</h3>
+                                <h3 style="font-size:13px">- Kirim email ke: support@pengaduansampah.com</h3>
+                                <h3 style="font-size:13px">- Hubungi hotline: 0800-123-4567</h3>
+                            </div>
+                            <!-- Message End -->
+                        </a>
+                        <div class="dropdown-divider"></div>
+                    </div>
+                </li>
+                <!-- Notifications Dropdown Menu -->
             </ul>
         </nav>
         <!-- /.navbar -->
